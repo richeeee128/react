9f87c45a4e606682618f2628d9e7ac1fd8075649
richeeee128 <qoqw808@gmail.com>
2023-07-10 22:00:24 +0900

    feat : 버튼,인풋 구현 완료

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from './component/Button';
+import Modal from './component/Modal';
+import Input from './component/Input';
+import Select from './component/Select';
+
+function App() {
+  return (
+    <>
+      <Button />
+      <Input />
+      <Modal />
+      <Select />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Input.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { NumericFormat } from 'react-number-format';
+import styled from 'styled-components';
+import { PrimaryBtn } from './Button';
+
+function Input() {
+  const [value, setValue] = useState('');
+  const [name, setName] = useState('');
+
+  // 콜백을 이용해 값을 가져오기
+  const onChangeValueHandler = (values) => {
+    setValue(values.value);
+  };
+
+  // 이름 값 가져오기
+  const onChangeNameHandler = (e) => {
+    setName(e.target.value);
+  };
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    if (name === '') {
+      alert('이름과 가격 모두 입력해주세요.');
+      return;
+    }
+    alert(`{name: ${name}, price: ${value} }`);
+    // 초기화
+    setName('');
+    setValue('');
+  };
+
+  return (
+    <Container>
+      <h1>Input</h1>
+      <form>
+        <Label> 이름 : </Label>
+        <StInput>
+          <input
+            type='text'
+            placeholder='입력해주세요'
+            value={name}
+            onChange={onChangeNameHandler}
+          />
+        </StInput>
+        <Label> 가격 : </Label>
+        {/* 넘버 포맷 패키지를 이용해 천 단위 콤마 넣기 */}
+        <StInput>
+          <NumericFormat
+            placeholder='0'
+            thousandSeparator={true}
+            value={value}
+            onValueChange={onChangeValueHandler}
+          />
+        </StInput>
+        <PrimaryBtn bg='primary' size='small' onClick={onClickHandler}>
+          저장
+        </PrimaryBtn>
+      </form>
+    </Container>
+  );
+}
+
+export default Input;
+
+const Container = styled.div`
+  padding: 10px;
+  height: 130px;
+`;
+
+const Label = styled.label`
+  margin-right: 5px;
+  font-weight: bold;
+`;
+
+const StInput = styled.span`
+  padding-right: 10px;
+  input {
+    width: 150px;
+    height: 28px;
+    border: none;
+    border-bottom: 2px solid #ccc;
+    outline: none;
+  }
+  input:focus {
+    border-bottom-color: #669eff;
+  }
+`;
